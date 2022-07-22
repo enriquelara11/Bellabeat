2ad2f21ca6f2cde0a01b8caa346b9e79d8540e73
Enrique Lara <77639314+enriquelara11@users.noreply.github.com>
2022-07-21 22:44:16 -0400

    Update Bellabeat_Analysis.sql

--- Bellabeat_Analysis.sql
+++ Bellabeat_Analysis.sql
@@ -29,9 +29,21 @@ FROM dailyactivity_merged.dailyActivity_merged -- 7671 Steps
 SELECT ROUND(AVG(TotalDistance)) as avg_distance
 FROM dailyactivity_merged.dailyActivity_merged -- 6 Miles
 
--- Entries by day of the week:
--- Sunday=1...Saturday=7
-SELECT
+-- 7. Entries recorded by day of the week:
+---  Sunday=1...Saturday=7
+SELECT 
+  CASE -- Case statement to convert a numeric date to text, such as 'Sun'... etc
+  WHEN day_of_week = 1 THEN "SUN"
+  WHEN day_of_week = 2 THEN "MON"
+  WHEN day_of_week = 3 THEN "TUE"
+  WHEN day_of_week = 4 THEN "WED"
+  WHEN day_of_week = 5 THEN "THU"
+  WHEN day_of_week = 6 THEN "FRI"
+  WHEN day_of_week = 7 THEN "SAT"
+  END AS Day, 
+  entries
+FROM
+  ( SELECT
   EXTRACT(DAYOFWEEK FROM DATE(ActivityDate)) AS day_of_week,
   COUNT(Id) as entries
 FROM
@@ -39,7 +51,52 @@ FROM
 GROUP BY
   day_of_week
 ORDER BY
-  day_of_week
+  day_of_week)
+--- The results show that TUE and WED are days when our users use the device the most, SUN and MON are the days the device is used the least
+  
+-- 8. View daily usage by reviewing the number of hours sleeping and subtracting it by 24.
+SELECT 
+  CASE 
+  WHEN day_week = 1 THEN "SUN"
+  WHEN day_week = 2 THEN "MON"
+  WHEN day_week = 3 THEN "TUE"
+  WHEN day_week = 4 THEN "WED"
+  WHEN day_week = 5 THEN "THU"
+  WHEN day_week = 6 THEN "FRI"
+  WHEN day_week = 7 THEN "SAT"
+  END AS Day,
+  avg_activity_day
+FROM (
+  SELECT
+  EXTRACT(DAYOFWEEK FROM DATE(SleepDate)) AS day_week,
+  ROUND((24-AVG(total_hours_sleep)),2) AS avg_activity_day -- nested query in order to find the total activity of the day
+  FROM (
+    SELECT
+    sleep.SleepDate,
+    ROUND((sleep.TotalMinutesAsleep/60),2) AS total_hours_sleep -- nested query in order to find the sleeping time recorded by our users
+    FROM `bellabeat-device-analysis.dailyactivity_merged.sleepDay_merged` sleep
+    LEFT JOIN `bellabeat-device-analysis.dailyactivity_merged.dailyActivity_merged` daily -- LEFT JOIN to combine Sleeping and Weight databases
+    ON sleep.Id = daily.Id AND sleep.SleepDate = daily.ActivityDate
+)
+GROUP BY day_week
+ORDER BY day_week
+)
+--- We've discovered that our users sleep more on weekends and register less activity.
+
+-- 9. Daily breakdown of our users activity level throughout the day as percentages
+SELECT 
+ROUND((ROUND(AVG(very_active_prcnt))/84)*100,1) AS avg_very_actve_percent, -- results are divided by 84 becasue 
+ROUND((ROUND(AVG(fairly_active_prcnt))/84)*100,1) AS avg_fairly_active_percent,
+ROUND((ROUND(AVG(lightly_active_prcnt))/84)*100,1) AS avg_lightly_active_percent,
+ROUND((ROUND(AVG(sedentary_minutes_prcnt))/84)*100,1) AS avg_sedentary_minutes_percent,
+FROM (
+  SELECT
+  ROUND(((VeryActiveMinutes/1440)*100),2) AS very_active_prcnt,
+  ROUND(((FairlyActiveMinutes/1440)*100),2) AS fairly_active_prcnt,
+  ROUND(((LightlyActiveMinutes/1440)*100),2) AS lightly_active_prcnt,
+  ROUND(((SedentaryMinutes/1440)*100),2) AS sedentary_minutes_prcnt,
+FROM dailyactivity_merged.dailyActivity_merged 
+)
 
 
 
